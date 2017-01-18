41ce28e71636b660dc27e5b8e102a6f39acfbaa7
Jad Joubran <joubran.jad@gmail.com>
2017-01-18 16:32:22 +0200

    Update service worker

--- precache-config.js
+++ precache-config.js
@@ -4,11 +4,13 @@ module.exports = {
 		"/?homescreen=1",
 		"index.html",
 		"index.html?homescreen=1",
-		"images/**"
+		"images/**",
+		"js/app.js",
+		"css/app.css"
 	],
+	ignoreUrlParametersMatching: [/^utm_/, /^homescreen/],
 	runtimeCaching: [{
 	   urlPattern: /jsonplaceholder\.typicode\.com\/users/,
-	   handler: 'fastest',
-	   debug: true,
+	   handler: 'fastest'
 	 }]
 };
